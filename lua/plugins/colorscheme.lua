47c8da0269a75ce18e74226fdc5716a4971aa487
Jay Barbhaiya <jay.barbhaiya@sap.com>
2023-06-16 10:18:19 +0200

    update colorscheme to catppuccin-latte

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add catppuccin/nvim
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    }
+  }
+}
