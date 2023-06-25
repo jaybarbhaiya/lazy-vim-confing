901e00197762829cef459617c670faa75b2c17a7
Jay Barbhaiya <jaybarb1990@gmail.com>
2023-06-25 23:32:28 +0200

    Add support for crates autocomplete

---
+++ lua/plugins/crates.lua
@@ -0,0 +1,12 @@
+-- Enable support for crates autocomplete
+return {
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+}
