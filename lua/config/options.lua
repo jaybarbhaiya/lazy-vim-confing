501915a25ee4bada7600c2e924b1963100ef475e
Jay Barbhaiya <jaybarb1990@gmail.com>
2023-08-26 04:01:57 +0200

    Options for indentation
    Add option for indentation for 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
