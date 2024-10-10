4ece73505058f69b04b6ba7e16648852d3c1944d
Jay Barbhaiya <jay.barbhaiya@sap.com>
2024-10-10 13:50:43 +0200

    add keymap for trim whitespaces

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>w", ":w<CR>") -- save buffer
-vim.keymap.set("n", "<leader>bd", ":bd<CR>") -- close buffer
+-- save buffer
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+
+-- close buffer
+vim.keymap.set("n", "<leader>bd", ":bd<CR>")
+
+-- Trim trailing whitespace
+vim.keymap.set("n", "<leader>kx", function()
+  vim.cmd("silent! keeppatterns %s/\\s\\+$//e")
+end, {})
